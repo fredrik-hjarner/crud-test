7ee7f69bd29b5a2b76af6c0ca59eb80ecc7c53db
fredrik-hjarner <frederick.hjarner@gmail.com>
2018-07-27 22:23:38 +0200

    Added in awesome file database

--- main.go
+++ main.go
@@ -3,12 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/peterbourgon/diskv"
 )
 
+func routesHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	switch path {
+	case "/value":
+		valueRoute(w, r)
+	default:
+		fmt.Fprintf(w, "Unrecognized path: %s", path)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	// Simplest transform function: put all the data files into the base dir.
+	// flatTransform := func(s string) []string { return []string{} }
+
+	// Initialize a new diskv store, rooted at "my-data-dir", with a 1MB cache.
+	d := diskv.New(diskv.Options{
+		BasePath: "my-data-dir",
+		// Transform:    flatTransform,
+		CacheSizeMax: 1024 * 1024,
 	})
+
+	// Write three bytes to the key "alpha".
+	key := "alpha"
+	d.Write(key, []byte{'1', '2', '3'})
+
+	// Read the value back out of the store.
+	value, _ := d.Read(key)
+	fmt.Printf("%v\n", value)
+
+	// Erase the key+value from the store (and the disk).
+	// d.Erase(key)
+
+	http.HandleFunc("/", routesHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
---
+++ value-route.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// value path
+
+func valueRoute(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+	if method == "GET" { // TODO: replace with switch.
+		getHandler(w, r)
+	} else if method == "POST" {
+		postHandler(w, r)
+	} else if method == "DEL" {
+		postHandler(w, r)
+	}
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	id := query.Get("id")
+	log.Printf("value=%s", id)
+	if id == "" {
+		// return all key-value pairs.
+		fmt.Fprintf(w, "Requested all values")
+	} else {
+		// return only the value of `id`.
+		fmt.Fprintf(w, "Requested one value")
+	}
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	// body := r.Body
+	query := r.URL.Query()
+	id := query.Get("id")
+	value := query.Get("value")
+	if id == "" || value == "" {
+		fmt.Fprintf(w, "Error: id and value is required.")
+	} else {
+		fmt.Fprintf(w, "Trying to store { %s: %s }", id, value)
+	}
+}
+
+func delHandler(w http.ResponseWriter, r *http.Request) {
+	// body := r.Body
+	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+}
