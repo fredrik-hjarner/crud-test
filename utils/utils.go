98e04700216cb14e64060be3926b803af76976b0
fredrik-hjarner <frederick.hjarner@gmail.com>
2020-07-03 12:23:54 +0200

    removed Filter function and use go-funk package instead.

--- utils/utils.go
+++ utils/utils.go
@@ -5,42 +5,39 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/thoas/go-funk"
 )
 
-func Filter(vs []string, f func(string) bool) []string {
-	vsf := make([]string, 0)
-	for _, v := range vs {
-		if f(v) {
-			vsf = append(vsf, v)
-		}
-	}
-	return vsf
-}
-
+// ListKeys Return all keys
 func ListKeys() []string {
 	var slice []string
 
-	err := filepath.Walk("data-dir",
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			key := strings.ReplaceAll(path, "\\", "/")
-			key = strings.TrimPrefix(key, "data-dir")
-			key = strings.TrimPrefix(key, "/")
-			if key != "" && info.IsDir() == false {
-				slice = append(slice, key)
-			}
-			return nil
-		})
+	walker := func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		key := strings.ReplaceAll(path, "\\", "/")
+		key = strings.TrimPrefix(key, "data-dir")
+		key = strings.TrimPrefix(key, "/")
+		if key != "" && info.IsDir() == false {
+			slice = append(slice, key)
+		}
+		return nil
+	}
+
+	err := filepath.Walk("data-dir", walker)
 	if err != nil {
 		log.Println(err)
 	}
 	return slice
 }
 
+// ListKeysWithPrefix Return the keys, of all keys, that start with prefix.
 func ListKeysWithPrefix(prefix string) []string {
-	return Filter(ListKeys(), func(v string) bool {
-		return strings.HasPrefix(v, prefix)
-	})
+	hasPrefix := func(key string) bool {
+		return strings.HasPrefix(key, prefix)
+	}
+
+	return funk.FilterString(ListKeys(), hasPrefix)
 }
