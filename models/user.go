72949671a86b0291b4610f284dd6375ad6ffeb10
fredrikhjarner <fredrik.hjarner@interactivesolutions.se>
2020-09-29 13:30:18 +0200

    Added validation tags to User model.

--- models/user.go
+++ models/user.go
@@ -2,8 +2,8 @@ package models
 
 // User model
 type User struct {
-	ID        string `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
+	ID        string `json:"id" valid:"required,stringlength(1|255)"`
+	FirstName string `json:"firstName" valid:"required,alpha,stringlength(1|255)"`
+	LastName  string `json:"lastName" valid:"required,alpha,stringlength(1|255)"`
+	Email     string `json:"email" valid:"required,email"`
 }
